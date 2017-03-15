cf220d93acf697f973dce3e9f8a99a012e36ad32
stevenreed <stevenreed@templatemonster.me>
2017-03-15 14:20:54 +0200

    debug social proof.PTR-2225

--- src/components/social_proof/SocialProof.jsx
+++ src/components/social_proof/SocialProof.jsx
@@ -132,6 +132,7 @@ export default class SocialProof extends React.Component {
       item.icon = item.icon || false;
       return item;
     });
+    console.log('state', state);
     this.setState(state);
   };
 
