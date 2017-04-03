2b7fea6a38c71a6700498f262889782619a5df9f
stevenreed <stevenreed@templatemonster.me>
2017-04-03 17:59:35 +0300

    debug.PTR-2225

--- src/utils/reactRemoteControl.js
+++ src/utils/reactRemoteControl.js
@@ -12,13 +12,10 @@ export default class ReactRemoteControl {
     this.componentName = componentName;
     this.locale = locale;
     this.path = path;
-    console.log('123132123', 123132123);
     this.renderWrap();
-    console.log('12313212dfhsgfhdgfhdghdfghdfghdfg');
   }
 
   show = (whereRenderClass, props = {}) => {
-    console.log('0');
     this.container = document.querySelector(whereRenderClass);
 
     /* if wrapper is undefined */
@@ -27,7 +24,6 @@ export default class ReactRemoteControl {
     } else {
       return false;
     }
-    console.log('1');
     // add callback for remove component
     props.afterRemoveDomNode = () => {
       if (typeof props.afterClose === 'function') {
@@ -36,13 +32,9 @@ export default class ReactRemoteControl {
       const node = document.getElementById(this.wrapper.id);
       node.parentNode.removeChild(node);
     };
-    console.log('2');
     const Component = this.components[this.componentName];
-    console.log('3');
     const localeService = new i18n.Factory(this.locale, this.path);
-    console.log('4');
     localeService.whenLocaleIsLoaded(provider => {
-      console.log('5');
       ReactDOM.render(
         <i18n.Provider i18n={provider}>
           <Component  {...props} />
