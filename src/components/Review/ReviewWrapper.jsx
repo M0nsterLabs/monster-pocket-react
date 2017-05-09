da521d81ff7a928b3aa3ec41710f0cd1413cf7d9
Viram <viram@templatemonster.me>
2017-05-09 09:26:36 +0300

    Added check to today or yesterday format date. PTR-145, PTR-152, PTR-3155

--- src/components/Review/ReviewWrapper.jsx
+++ src/components/Review/ReviewWrapper.jsx
@@ -394,6 +394,7 @@ export default class Reviews extends React.Component {
     const day  = time.getDate();
     const hour = time.getHours();
     const min = time.getMinutes() < 10 ? `0${time.getMinutes()}` : time.getMinutes();
+    const timeNowUTC = new Date();
     const timeNow = new Date().getTime() / 1000;
     const average =  timeNow - timestamp;
 
@@ -413,15 +414,15 @@ export default class Reviews extends React.Component {
     if (average < units.MINUTE * 2) {
       date = this.context.i18n.l('Now');
     } else if (average < units.HOUR) {
-      date = `${time.getMinutes()} ${this.context.i18n.l('minutes ago')}`;
+      date = `${min} ${this.context.i18n.l('minutes ago')}`;
     } else if (average < units.HOUR * 2) {
-      date = `${time.getHours()} ${this.context.i18n.l('hour ago')}`;
+      date = `${hour} ${this.context.i18n.l('hour ago')}`;
     } else if (average < units.HOUR * 10) {
-      date = `${time.getHours()} ${this.context.i18n.l('hours ago')}`;
-    }  else if (average < units.DAY * 1) {
-      date = `${this.context.i18n.l('Today at')} ${time.getHours()}:${time.getMinutes()}`;
-    }  else if (average < units.DAY * 2) {
-      date = `${this.context.i18n.l('Yesterday at')} ${time.getHours()}:${time.getMinutes()}`;
+      date = `${hour} ${this.context.i18n.l('hours ago')}`;
+    }  else if ((average < units.DAY * 1) && (timeNowUTC.toDateString() === time.toDateString())) {
+      date = `${this.context.i18n.l('Today at')} ${hour}:${min}`;
+    }  else if ((average < units.DAY * 2) && (timeNowUTC.toDateString() !== time.toDateString())) {
+      date = `${this.context.i18n.l('Yesterday at')} ${hour}:${min}`;
     } else {
       date = `${month} ${day}, ${year} ${this.context.i18n.l('at')} ${hour}:${min}`;
     }
