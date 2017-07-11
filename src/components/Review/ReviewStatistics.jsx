04d9ce81539d7c220175909495b3db8bd22a01d7
viram <viram@templatemonster.me>
2017-07-11 18:32:47 +0300

    Added reviews statistic. PLASMA-615, PLASMA-638

---
+++ src/components/Review/ReviewStatistics.jsx
@@ -0,0 +1,159 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StarsRating from 'quark/lib/StarsRating';
+
+let summPercent = 0;
+let percentReview = [];
+let iterator = 0;
+let lineBlockWidth = 650;
+
+export default class ReviewStatistics extends React.Component {
+  static contextTypes = {
+    i18n: PropTypes.object
+  };
+
+  static propTypes = {
+    countReview: PropTypes.array,
+    summReview: PropTypes.number,
+    averageRating: PropTypes.number,
+  };
+
+  componentWillMount () {
+    this.countPercents();
+  };
+
+  showStatisticsLine = (stars) => {
+    let {countReview} = this.props;
+    if (iterator >= 5) return;
+    iterator++;
+    if (countReview[iterator-1] === 0) return;
+
+    var widthLine,
+      color,
+      getMaxPercent = this.getMaxPercent();
+      widthLine = percentReview[iterator-1]*lineBlockWidth/getMaxPercent;
+
+    switch (true) {
+      case (percentReview[iterator-1] >= 0) && (percentReview[iterator-1] <= 9) :
+        color = "#1976D2";
+        break;
+      case (percentReview[iterator-1] >= 10) && (percentReview[iterator-1] <= 19) :
+        color = "#2196F3";
+        break;
+      case (percentReview[iterator-1] >= 20) && (percentReview[iterator-1] <= 29) :
+        color = "#42A5F5";
+        break;
+      case (percentReview[iterator-1] >= 30) && (percentReview[iterator-1] <= 39) :
+        color = "#64B5F6";
+        break;
+      case (percentReview[iterator-1] >= 40) && (percentReview[iterator-1] <= 49) :
+        color = "#90CAF9";
+        break;
+      case (percentReview[iterator-1] >= 50) && (percentReview[iterator-1] <= 59) :
+        color = "#FFE082";
+        break;
+      case (percentReview[iterator-1] >= 60) && (percentReview[iterator-1] <= 69) :
+        color = "#FFCA28";
+        break;
+      case (percentReview[iterator-1] >= 70) && (percentReview[iterator-1] <= 79) :
+        color = "#FFAB00";
+        break;
+      case (percentReview[iterator-1] >= 80) && (percentReview[iterator-1] <= 89) :
+        color = "#FF8F00";
+        break;
+      default :
+        color = "#FF6F00";
+        break;
+    }
+
+    return (
+        <div className="statistics__line-block js-statistic">
+          <div className="statistics__stars js-statistics-stars rating__stars rating-stars-block">
+            <StarsRating
+              defaultRating={stars}
+              disabled={true}
+            />
+            <span className="t5 js-statistic-count">{countReview[iterator-1]}</span>
+          </div>
+          <div className="statistics__line-wrap js-line-wrap" style={{"color": color}}>
+            <div className="statistics__line js-statistic-line" style={{"width": widthLine, "backgroundColor": color}}></div>
+            <span className="statistics__percent t5 js-statistic-percent" style={{"color": color}}>{percentReview[iterator-1]}% </span>
+          </div>
+        </div>
+    )
+  };
+
+  countPercents = () => {
+    this.props.countReview.map((countReviewItem, i) => {
+      percentReview[i]=Math.round(countReviewItem*100/this.props.summReview);
+      summPercent+=percentReview[i];
+    });
+    let diffSummPercent = summPercent - 100;
+    if (diffSummPercent === 0) return;
+    if (diffSummPercent > 0) {
+      for (let i = 4; i >= 0; i--) {
+        if (percentReview[i] != 0) {
+          percentReview[i] -= diffSummPercent;
+          break;
+        }
+      }
+    }
+    else {
+      for (let i = 0; i < 5; i++) {
+        if (percentReview[i] != 0) {
+          percentReview[i] -= diffSummPercent;
+          break;
+        }
+      }
+    }
+  };
+
+  lineBlockWidth = () => {
+    let reviewsStatisticsWidth = document.getElementsByClassName('js-reviews-statistics')[0].clientWidth - 40;
+    return document.getElementById('preview-page').classList.contains('item-page')
+      ? reviewsStatisticsWidth
+      : reviewsStatisticsWidth - document.getElementsByClassName('js-statistics-stars')[0].clientWidth;
+  };
+
+  getMaxPercent = () => {
+    let maxPercent = percentReview[0];
+    for (let i = 0; i < 5; i++) {
+      if (percentReview[i] > maxPercent) {
+        maxPercent = percentReview[i];
+      }
+    }
+    return maxPercent;
+  };
+
+  render() {
+    return (
+      <div className="reviews__infographics js-reviews-statistics" id="preview-page">
+        <div className="rating">
+          <h3 className="h3">{this.context.i18n.l('STATISTICS')}</h3>
+          <div className="rating__stars">
+            <span className="rating__text t1">
+              {this.context.i18n.l('Average rating')}:
+            </span>
+            <StarsRating
+              defaultRating={this.props.averageRating}
+              disabled={true}
+            />
+          </div>
+          <span className="rating__text_new-page h3">{this.context.i18n.l('Product rating')}:</span>
+
+        </div>
+
+
+        <span className="reviews__infographics-text-new-page t1">{this.context.i18n.l('INCLUDES')}:</span>
+
+        <div className="statistics">
+          {this.showStatisticsLine(5)}
+          {this.showStatisticsLine(4)}
+          {this.showStatisticsLine(3)}
+          {this.showStatisticsLine(2)}
+          {this.showStatisticsLine(1)}
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/Review/ReviewWrapper.jsx
+++ src/components/Review/ReviewWrapper.jsx
@@ -13,6 +13,7 @@ import ProductsData        from 'tm-products-api-client-js';
 
 import ReviewItem          from './ReviewItem';
 import ReviewEditor        from './ReviewEditor';
+import ReviewStatistics    from './ReviewStatistics';
 
 import {
   getCdnImageUrl,
@@ -59,7 +60,13 @@ export default class Reviews extends React.Component {
     countReviewOtherLocale : 0,
     otherLocale            : false,
     showMoreVisible        : false,
-    sort                   : '-id'
+    sort                   : '-id',
+    totalCount5: 0,
+    totalCount4: 0,
+    totalCount3: 0,
+    totalCount2: 0,
+    totalCount1: 0,
+    averageRating: 0,
   };
 
   constructor (props) {
@@ -403,6 +410,47 @@ export default class Reviews extends React.Component {
     return currentItemsCount === 0 || currentItemsCount < totalItemsCount;
   };
 
+  getCountScoreReviews = (score) => {
+    fetch(`${Config.reviewsServiceURL}reviews?template_id=${this.props.templateId}&score=${score}`, {
+      method: 'head',
+    })
+    .then(response => {
+      var key = `totalCount${score}`;
+      var obj  = {};
+      obj[key] = parseInt(response.headers.get('X-Pagination-Total-Count'), 10);
+      this.setState(obj);
+    })
+    .catch((error) => {
+      throw error;
+    });
+  };
+
+  findProperty = (property) => {
+    return property.propertyName === 'review_average_score'
+  };
+
+  getAverageRating = () => {
+    fetch(`${Config.productsServiceURL}products?ids=${this.props.templateId}`, {
+      method: 'get',
+    })
+    .then(getResponseJSON)
+    .then(data => {
+      this.setState({
+        averageRating: parseInt(data[0].properties.find(this.findProperty).value)
+      });
+    })
+    .catch((error) => {
+      throw error;
+    });
+  };
+
+  componentWillMount () {
+    for (let i=1; i<=5; i++) {
+      this.getCountScoreReviews(i);
+    }
+    this.getAverageRating();
+  };
+
   componentDidMount () {
     this.state.products.products = [];
     if (this.props.accessToken) {
@@ -496,7 +544,6 @@ export default class Reviews extends React.Component {
         items: []
       },
     }, () => {
-      console.log(this.state.otherLocale, this.iteratorLocale);
       this.getReviews(this.state.otherLocale ? '' : LOCALES[this.iteratorLocale]);
     })
   };
@@ -527,6 +574,7 @@ export default class Reviews extends React.Component {
   };
 
   render () {
+    let {totalCount5, totalCount4, totalCount3, totalCount2, totalCount1, averageRating} = this.state;
     return (
       <div className="page-content"><span className="reviews__count">{this.state.reviews.totalCount}</span>
         {
@@ -556,6 +604,12 @@ export default class Reviews extends React.Component {
                 )
               : (
                 <div className="reviews">
+                  <ReviewStatistics
+                    countReview={[totalCount5, totalCount4, totalCount3, totalCount2,totalCount1]}
+                    summReview={totalCount5+totalCount4+totalCount3+totalCount2+totalCount1}
+                    averageRating={averageRating}
+                  />
+
                   <div className="reviews__header">
                     <h2 className="h3"><span className="reviews__total-count">{this.state.reviews.totalCount}</span> {this.context.i18n.l(`REVIEWS & RATINGS`)}</h2>
                     {this.sortReviews()}
