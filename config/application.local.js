e022d132ea8571e600177304170c27b6b7f27eb6
stevenreed <stevenreed@templatemonster.me>
2017-08-16 10:34:48 +0300

    PLASMA-1092 - formatting config & add new word

--- config/application.local.js
+++ config/application.local.js
@@ -1,16 +1,16 @@
 const baseConfig = require('./application.base.js');
 export default {
   ...baseConfig.default,
-  authServiceURL              : '//api.templatemonsterdev.com/oauth/',
-  accountServiceURL           : '//api.templatemonsterdev.com/users/v1/',
-  uploaderServiceURL          : '//api.templatemonsterdev.com/uploads/v1/',
-  productsServiceURL          : '//api.templatemonsterdev.com/products/v1/',
-  authorServiceURL            : '//api.templatemonsterdev.com/authors/v1/',
-  ordersServiceURL            : '//api.templatemonsterdev.com/orders/v1/',
-  reviewsServiceURL           : '//api.templatemonsterdev.com/reviews/v1/',
-  balancesServiceURL          : '//api.templatemonsterdev.com/balances/v1/',
-  monsterURL                  : '//www.templatemonsterdev.com/',
-  chatURL                     : '//chat.template-help.dev/',
-  tmimgcdnURL                 : '//s.tmimgcdn.com/scr/400x400/',
-  facebookAppId               : '1449028418459900'
+  authServiceURL: '//api.templatemonsterdev.com/oauth/',
+  accountServiceURL: '//api.templatemonsterdev.com/users/v1/',
+  uploaderServiceURL: '//api.templatemonsterdev.com/uploads/v1/',
+  productsServiceURL: '//api.templatemonsterdev.com/products/v1/',
+  authorServiceURL: '//api.templatemonsterdev.com/authors/v1/',
+  ordersServiceURL: '//api.templatemonsterdev.com/orders/v1/',
+  reviewsServiceURL: '//api.templatemonsterdev.com/reviews/v1/',
+  balancesServiceURL: '//api.templatemonsterdev.com/balances/v1/',
+  monsterURL: '//www.templatemonsterdev.com/',
+  chatURL: '//chat.template-help.dev/',
+  tmimgcdnURL: '//s.tmimgcdn.com/scr/400x400/',
+  facebookAppId: '1449028418459900'
 };
